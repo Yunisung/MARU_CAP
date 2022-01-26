f763fc03c220f1b2853b93055a68e3735b04060f
pys890102 <pys890102@bkwinnners.com>
2022-01-26 16:48:50 +0900

    lib최신화, monitor.sh 경로 변경

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -3,7 +3,7 @@
 #아래 모듈은 MARU_CAP 이 데몬이기때문에 언제든지 죽을 수 있는 위험이 있다.
 #그리하여 10분에 한번씩 PID 가 살아 있는지 확인 후 없을 경우 실행할 수 있도록 되어 있다.
 
-cd /home/kown/MARU_CAP/bin
+cd /home/bkwinners/MARU/MARU_CAP/bin
 
 pid_file="daemon.pid"
 
