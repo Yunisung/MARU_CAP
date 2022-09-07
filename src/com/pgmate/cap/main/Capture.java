c2e2d2e6103776022a18df7a0a5631f295ec3c4b
pys890102 <pys890102@bkwinnners.com>
2022-09-07 14:40:15 +0900

    수수료 반올림 취소

--- src/com/pgmate/cap/main/Capture.java
+++ src/com/pgmate/cap/main/Capture.java
@@ -1184,17 +1184,11 @@ public class Capture {
 	}
 	
 	public long calcVat(long amount){
-		//220905_PYS : 수수료 반올림 적용
-		double vat = amount * 0.1f;
-		vat = Math.round(vat);
-		return new Double(vat).longValue();
-
-//		if(amount < 0){
-//			return -new Double(-amount *10 /100).longValue();
-//
-//		}else{
-//			return new Double(amount *10 /100).longValue();
-//		}
+		if(amount < 0){
+			return -new Double(-amount *10 /100).longValue();
+		}else{
+			return new Double(amount *10 /100).longValue();
+		}
 	}
 	
 	public long calcFeeVat(long amount,double rate){
