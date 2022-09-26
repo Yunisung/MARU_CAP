58a87bcf765275414c78770445d49c2da9c47425
kjm <jjggooo>
2022-09-26 15:54:46 +0900

    리스크 버그 추가 커밋
    (cherry picked from commit fd5e7c69b4b26efd1b1379e483b81adbe1631488)

--- src/com/pgmate/cap/main/Capture.java
+++ src/com/pgmate/cap/main/Capture.java
@@ -819,8 +819,8 @@ public class Capture {
 	 * 위험거래로 변경한다.
 	 * @param capId
 	 */
-	public void warning(String capId){
-		SharedMap<String,Object> rootCapMap	 	= trxDAO.getTrxCapId(capId);
+	public void warning(String trxId){
+		SharedMap<String,Object> rootCapMap	 	= trxDAO.getTrxCapId(trxId);
 		if(rootCapMap == null){
 			//검색된 매입내역이 없으면 리턴한다.
 			return;
