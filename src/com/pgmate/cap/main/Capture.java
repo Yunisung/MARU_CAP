21d3cf1cf7ad9d693aa792f5dab54b0063a71add
kjm <jjggooo>
2024-03-07 14:25:32 +0900

    월세앱 - 월세앱 들어오는지 확인 로그

--- src/com/pgmate/cap/main/Capture.java
+++ src/com/pgmate/cap/main/Capture.java
@@ -250,6 +250,8 @@ public class Capture {
 
 		// 서비스구분: 외부서비스(월세앱) 사용유무
 		boolean isRentApp = "사용".equals(mchtSvcMap.getString("rent")) && !trxRentMap.isNullOrSpace("rentId");
+		logger.info("mcht svc rent : {}", mchtSvcMap.getString("rent"));
+		logger.info("rentId : {}", trxRentMap.isNullOrSpace("rentId"));
 		if (isRentApp) {
 			trxCapMap.put("serviceType", "월세앱");
 		} else {
