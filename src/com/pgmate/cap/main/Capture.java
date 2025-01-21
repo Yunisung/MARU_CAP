34fdfeeff93546a1ae47c56e2b41285e562f0d55
pys890102 <pys890102@bkwinners.com>
2025-01-21 09:48:42 +0900

    240121_월세앱 체크 수정 (2)

--- src/com/pgmate/cap/main/Capture.java
+++ src/com/pgmate/cap/main/Capture.java
@@ -249,13 +249,15 @@ public class Capture {
 		}
 
 		// 서비스구분: 외부서비스(월세앱) 사용유무
-		boolean isRentApp = "사용".equals(mchtSvcMap.getString("rent")) && trxRentMap != null && !trxRentMap.isNullOrSpace("rentId");
-		logger.info("mcht svc rent : {}", mchtSvcMap.getString("rent"));
-		logger.info("rentId : {}", trxRentMap.isNullOrSpace("rentId"));
-		if (isRentApp) {
-			trxCapMap.put("serviceType", "월세앱");
-		} else {
-			trxCapMap.put("serviceType", "");
+		if(trxRentMap != null) {
+			boolean isRentApp = "사용".equals(mchtSvcMap.getString("rent")) && !trxRentMap.isNullOrSpace("rentId");
+			logger.info("mcht svc rent : {}", mchtSvcMap.getString("rent"));
+			logger.info("rentId : {}", trxRentMap.isNullOrSpace("rentId"));
+			if (isRentApp) {
+				trxCapMap.put("serviceType", "월세앱");
+			} else {
+				trxCapMap.put("serviceType", "");
+			}
 		}
 
 		trxCapMap.put("cardId", trxPayMap.getString("cardId"));
