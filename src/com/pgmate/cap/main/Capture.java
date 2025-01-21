04fa7e0f953c3e66ab308acfad2449eeadccefa3
pys890102 <pys890102@bkwinners.com>
2025-01-21 09:44:49 +0900

    250121_월세앱 체크 수정

--- src/com/pgmate/cap/main/Capture.java
+++ src/com/pgmate/cap/main/Capture.java
@@ -249,7 +249,7 @@ public class Capture {
 		}
 
 		// 서비스구분: 외부서비스(월세앱) 사용유무
-		boolean isRentApp = "사용".equals(mchtSvcMap.getString("rent")) && !trxRentMap.isNullOrSpace("rentId");
+		boolean isRentApp = "사용".equals(mchtSvcMap.getString("rent")) && trxRentMap != null && !trxRentMap.isNullOrSpace("rentId");
 		logger.info("mcht svc rent : {}", mchtSvcMap.getString("rent"));
 		logger.info("rentId : {}", trxRentMap.isNullOrSpace("rentId"));
 		if (isRentApp) {
